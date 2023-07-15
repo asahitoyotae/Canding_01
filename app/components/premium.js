49b9228bd7d825869ebc4b1f9021950e01874748
asahitoyotae <asahitoyotae@gmail.com>
2023-07-15 23:12:20 +0800

    new Commit

--- app/components/premium.js
+++ app/components/premium.js
@@ -101,165 +101,167 @@ const Premium = () => {
       />
 
       <div
-        className={`shopping_container ${
+        className={` new_cart ${
           showPaypal == true ? "shopping_active" : "shopping_inactive"
         }`}
       >
-        <div
-          className="grid grid-cols-9 gap-4 duration-500 w-full py-4 px-3 text-black my-3 rounded-md ${
+        <div className="shopping_container ">
+          <div
+            className="grid grid-cols-9 gap-4 duration-500 w-full py-4 px-3 text-black my-3 rounded-md ${
             bg-gray-100"
-        >
-          <div className="col-span-2 flex items-center justify-center">
-            <FontAwesomeIcon
-              icon={faFrog}
-              style={{ width: "50px", height: "50px" }}
-            />
-          </div>
-          <div className="catalog_mid col-span-5 text-sm ">
-            <p>Frog </p>
-            <p>Free account is limited for up to 3 request only.</p>
-          </div>
-          <div className="col-span-2 text-xl font-bold text-center flex items-center justify-center"></div>
-        </div>
-        <div
-          onClick={() => setAmount(5)}
-          className={`relative grid grid-cols-9 gap-4 duration-500 w-full py-4 px-3 text-black my-3 rounded-md ${
-            amount == 5 ? "bg-orange-200" : "bg-gray-100"
-          }`}
-        >
-          <div className="col-span-2 flex justify-center">
-            <FontAwesomeIcon
-              icon={faDove}
-              style={{ width: "50px", height: "50px" }}
-            />
-          </div>
-          <div className="catalog_mid col-span-5 text-sm ">
-            <p>Dove</p>
-            <p>
-              Ideal for students in need of writing assignments, copywriting,
-              article writing and more.
-            </p>
-          </div>
-          <div className="col-span-2 text-xl font-bold text-center flex items-center justify-center">
-            7 Days
-          </div>
-          <div className="price">5.00 usd</div>
-        </div>
-        <div
-          onClick={() => setAmount(17)}
-          className={`relative grid grid-cols-9 gap-4 duration-500 w-full py-4 px-3 text-black my-3 rounded-md ${
-            amount == 17 ? "bg-orange-200" : "bg-gray-100"
-          }`}
-        >
-          <div className="col-span-2 flex justify-center flex-col">
-            <FontAwesomeIcon
-              icon={faDragon}
-              style={{ width: "50px", height: "50px" }}
-            />{" "}
-          </div>
-          <div className="catalog_mid col-span-5 text-sm ">
-            <p>Dragon </p>
-            <p>
-              Perfect for Students and Individuals engage in article writting,
-              copywritng etc. Best for developers.
-            </p>
-          </div>
-          <div className="col-span-2 text-xl font-bold text-center flex items-center justify-center">
-            30 Days
-          </div>
-          <div className="price">17.00 usd</div>
-        </div>
-        <p className="terms_memo">
-          by proceeding to payment you accept our{" "}
-          <span
-            onClick={() => {
-              console.log("terms");
-              setShowTerms(true);
-            }}
           >
-            Terms and Conditions
-          </span>
-        </p>
-        <div className="mt-7 w-full">
-          <PayPalScriptProvider
-            options={{
-              clientId:
-                "AQa2AA9WcI2rHiUzdfZ1uRnqQdMf0KaKl3tQ-P3ELuvUBS-ca6gN8_Hz8iZbonq9pMOE9sMZUTzRqvSP",
-            }}
+            <div className="col-span-2 flex items-center justify-center">
+              <FontAwesomeIcon
+                icon={faFrog}
+                style={{ width: "50px", height: "50px" }}
+              />
+            </div>
+            <div className="catalog_mid col-span-5 text-sm ">
+              <p>Frog </p>
+              <p>Free account is limited for up to 3 request only.</p>
+            </div>
+            <div className="col-span-2 text-xl font-bold text-center flex items-center justify-center"></div>
+          </div>
+          <div
+            onClick={() => setAmount(5)}
+            className={`relative grid grid-cols-9 gap-4 duration-500 w-full py-4 px-3 text-black my-3 rounded-md ${
+              amount == 5 ? "bg-orange-200" : "bg-gray-100"
+            }`}
           >
-            <PayPalButtons
-              style={{ layout: "vertical", color: "white" }}
-              disabled={false}
-              forceReRender={[amount]}
-              fundingSource={undefined}
-              createOrder={(data, actions) => {
-                return actions.order
-                  .create({
-                    purchase_units: [
-                      {
-                        amount: {
-                          currency_code: "USD",
-                          value: `${amount}`,
+            <div className="col-span-2 flex justify-center">
+              <FontAwesomeIcon
+                icon={faDove}
+                style={{ width: "50px", height: "50px" }}
+              />
+            </div>
+            <div className="catalog_mid col-span-5 text-sm ">
+              <p>Dove</p>
+              <p>
+                Ideal for students in need of writing assignments, copywriting,
+                article writing and more.
+              </p>
+            </div>
+            <div className="col-span-2 text-xl font-bold text-center flex items-center justify-center">
+              7 Days
+            </div>
+            <div className="price">5.00 usd</div>
+          </div>
+          <div
+            onClick={() => setAmount(17)}
+            className={`relative grid grid-cols-9 gap-4 duration-500 w-full py-4 px-3 text-black my-3 rounded-md ${
+              amount == 17 ? "bg-orange-200" : "bg-gray-100"
+            }`}
+          >
+            <div className="col-span-2 flex justify-center flex-col">
+              <FontAwesomeIcon
+                icon={faDragon}
+                style={{ width: "50px", height: "50px" }}
+              />{" "}
+            </div>
+            <div className="catalog_mid col-span-5 text-sm ">
+              <p>Dragon </p>
+              <p>
+                Perfect for Students and Individuals engage in article writting,
+                copywritng etc. Best for developers.
+              </p>
+            </div>
+            <div className="col-span-2 text-xl font-bold text-center flex items-center justify-center">
+              30 Days
+            </div>
+            <div className="price">17.00 usd</div>
+          </div>
+          <p className="terms_memo">
+            by proceeding to payment you accept our{" "}
+            <span
+              onClick={() => {
+                console.log("terms");
+                setShowTerms(true);
+              }}
+            >
+              Terms and Conditions
+            </span>
+          </p>
+          <div className="mt-7 w-full">
+            <PayPalScriptProvider
+              options={{
+                clientId:
+                  "AQa2AA9WcI2rHiUzdfZ1uRnqQdMf0KaKl3tQ-P3ELuvUBS-ca6gN8_Hz8iZbonq9pMOE9sMZUTzRqvSP",
+              }}
+            >
+              <PayPalButtons
+                style={{ layout: "vertical", color: "white" }}
+                disabled={false}
+                forceReRender={[amount]}
+                fundingSource={undefined}
+                createOrder={(data, actions) => {
+                  return actions.order
+                    .create({
+                      purchase_units: [
+                        {
+                          amount: {
+                            currency_code: "USD",
+                            value: `${amount}`,
+                          },
                         },
-                      },
-                    ],
-                  })
-                  .then((orderId) => {
-                    //Your code here after create the order
-                    setService(amount);
-                    return orderId;
-                  });
-              }}
-              onApprove={function (data, actions) {
-                return actions.order.capture().then(function (event) {
-                  const paidAmount = event.purchase_units[0].amount.value;
-                  const payload = {
-                    orderId: data.orderID,
-                    payerId: data.payerID,
-                    amount: paidAmount,
-                  };
-                  const days = paidAmount == 17 ? "30 days" : "7 days";
-                  const secret = process.env.NEXT_PUBLIC_AUTH_KEY_VALID;
-                  const token = jwt.sign(
-                    payload,
-                    secret,
-                    { expiresIn: days },
-                    (error, token) => {
-                      if (error) {
-                        console.log(error);
-                      } else {
-                        console.log("token", token);
-                        localStorage.setItem("__validity__", token);
-                        setToken(token);
-                        setShowPaypal(false);
-                        setService(event.purchase_units[0].amount.value);
-                        setUserIcon({
-                          icon: faMedal,
-                          color: "#f98433",
-                        }); // Your code here after capture the order
+                      ],
+                    })
+                    .then((orderId) => {
+                      //Your code here after create the order
+                      setService(amount);
+                      return orderId;
+                    });
+                }}
+                onApprove={function (data, actions) {
+                  return actions.order.capture().then(function (event) {
+                    const paidAmount = event.purchase_units[0].amount.value;
+                    const payload = {
+                      orderId: data.orderID,
+                      payerId: data.payerID,
+                      amount: paidAmount,
+                    };
+                    const days = paidAmount == 17 ? "30 days" : "7 days";
+                    const secret = process.env.NEXT_PUBLIC_AUTH_KEY_VALID;
+                    const token = jwt.sign(
+                      payload,
+                      secret,
+                      { expiresIn: days },
+                      (error, token) => {
+                        if (error) {
+                          console.log(error);
+                        } else {
+                          console.log("token", token);
+                          localStorage.setItem("__validity__", token);
+                          setToken(token);
+                          setShowPaypal(false);
+                          setService(event.purchase_units[0].amount.value);
+                          setUserIcon({
+                            icon: faMedal,
+                            color: "#f98433",
+                          }); // Your code here after capture the order
+                        }
                       }
-                    }
-                  );
-                });
+                    );
+                  });
+                }}
+              />
+            </PayPalScriptProvider>
+          </div>
+          <div className="flex w-full">
+            <div className="flex-1"></div>
+            <button
+              className="back_button"
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                setShowPaypal(false);
               }}
-            />
-          </PayPalScriptProvider>
-        </div>
-        <div className="flex w-full">
-          <div className="flex-1"></div>
-          <button
-            className="back_button"
-            onClick={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
-              setShowPaypal(false);
-            }}
-          >
-            <FontAwesomeIcon
-              icon={faChevronRight}
-              style={{ width: "20px", height: "20px", color: "black" }}
-            />
-          </button>
+            >
+              <FontAwesomeIcon
+                icon={faChevronRight}
+                style={{ width: "20px", height: "20px", color: "black" }}
+              />
+            </button>
+          </div>
         </div>
       </div>
       {showPaypal && (
